d984cee5266088262916a1df1dc23746f654a44f
Chanakya <chanakya.valluri@gmail.com>
2019-09-06 14:09:54 -0700

    added methods initialize, parent=, add_child and remove_child

--- W3D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,5 +1,30 @@
 class PolyTreeNode
 
-  
+    attr_reader :value, :parent, :children
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        unless node.nil?
+            @parent.children.delete(self) unless @parent.nil?
+            @parent = node
+            node.children << self unless node.children.include?(self)
+        else
+            @parent = nil
+        end
+    end
+
+    def add_child(node)
+        node.parent = self
+        @children << node unless @children.include?(node)
+    end
+    
+    def remove_child(node)
+        raise 'Given node is not a child' unless @children.include?(node)
+        node.parent = nil
+    end
 end
