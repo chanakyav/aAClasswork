a8f29abe74847dc50f40bdfecdade40ae449c55a
Chanakya <chanakya.valluri@gmail.com>
2019-09-06 16:38:11 -0700

    added #initialize and renamed file names

---
+++ W3D5/KnightPathFinder/KnightPathFinder.rb
@@ -0,0 +1,26 @@
+require '../PolyTreeNode/lib/00_tree_node.rb'
+
+class KnightPathFinder
+
+    def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = nil
+
+    end
+
+    def find_path
+
+    end
+
+    def self.valid_moves(pos)
+
+    end
+
+    def build_move_tree
+
+    end
+
+    def new_move_positions
+
+    end
+end
