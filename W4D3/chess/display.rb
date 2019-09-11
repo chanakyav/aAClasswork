961c5d37eed052b2acbedafef5a9b347f1516677
Chanakya <chanakya.valluri@gmail.com>
2019-09-10 17:41:38 -0700

    init chess

---
+++ W4D3/chess/display.rb
@@ -0,0 +1,15 @@
+require_relative 'board'
+require_relative 'cursor'
+
+class Display
+  
+  def initialize(board)
+    @board = board
+    @cursor
+  end
+
+  def render
+    
+  end
+
+end
---
+++ W4D3/chess/human_player.rb
@@ -0,0 +1,9 @@
+require_relative 'player'
+
+class HumanPlayer < Player
+  
+  def make_board(_board)
+    
+  end
+
+end
