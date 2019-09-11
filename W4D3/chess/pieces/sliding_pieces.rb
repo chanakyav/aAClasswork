55cbd06d6a7374ffc9d969edfde044f7c9bb0302
Chanakya <chanakya.valluri@gmail.com>
2019-09-10 17:52:11 -0700

    init

---
+++ W4D3/chess/pieces/sliding_pieces.rb
@@ -0,0 +1,16 @@
+require '../piece.rb'
+require_relative 'slideable'
+
+class SlidingPieces < Piece
+  include Slideable
+
+  def symbol
+    
+  end
+
+  protected
+
+  def move_dirs
+    
+  end
+end
---
+++ W4D3/chess/pieces/stepping_pieces.rb
@@ -0,0 +1,17 @@
+require '../piece.rb'
+require_relative 'stepable'
+
+class SteppingPiece < Piece
+  include Stepable
+
+  def symbol
+    
+  end
+
+  protected
+  
+  def move_diffs
+    
+  end
+
+end
