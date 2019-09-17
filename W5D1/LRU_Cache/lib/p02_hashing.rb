746fe568dafb056ae19651c5d374bd7096cfe16b
Chanakya <chanakya.valluri@gmail.com>
2019-09-16 17:01:58 -0700

    init

---
+++ W5D1/LRU_Cache/lib/p02_hashing.rb
@@ -0,0 +1,24 @@
+class Integer
+  # Integer#hash already implemented for you
+end
+
+class Array
+  def hash
+    self.join('').to_i.hash
+  end
+end
+
+class String
+  def hash
+    chars = self.chars
+    
+  end
+end
+
+class Hash
+  # This returns 0 because rspec will break if it returns nil
+  # Make sure to implement an actual Hash#hash method
+  def hash
+    0
+  end
+end
