815b9769da1738ded8eaff20b02b2b1aeacd5010
Chanakya <chanakya.valluri@gmail.com>
2019-09-18 17:46:43 -0700

    working problem 2 solution

--- W5D3/whiteboard.rb
+++ W5D3/whiteboard.rb
@@ -11,3 +11,18 @@ def shorter_path(path)
 end
 
 p shorter_path("/users/app_academy_work/curriculumn/ruby/../sql/./../rails")
+
+
+def bounces(height, bounce, window)
+  if (height > 0 && bounce.between?(0,1) && window < height)
+    count = 0
+    while true
+      height *= bounce
+      return count if height < window
+      count += 1
+    end
+  end
+  -1
+end
+
+p bounces(6, 0.66, 1.5)
