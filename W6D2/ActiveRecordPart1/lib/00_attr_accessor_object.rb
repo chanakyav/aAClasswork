99b7b6382fac71ec158af43e53df64fbcafb26ef
Chanakya <chanakya.valluri@gmail.com>
2019-09-24 14:10:46 -0700

    comments

--- W6D2/ActiveRecordPart1/lib/00_attr_accessor_object.rb
+++ W6D2/ActiveRecordPart1/lib/00_attr_accessor_object.rb
@@ -3,12 +3,12 @@ class AttrAccessorObject
     names.each do |name|
       inst_var = '@'+name.to_s
 
-      define_method(name) do # Getters
+      define_method(name) do                # Getters
         instance_variable_get(inst_var)
       end
 
       sttr_name = (name.to_s + '=').to_sym
-      define_method(sttr_name) do |value| # Setters
+      define_method(sttr_name) do |value|   # Setters
         instance_variable_set(inst_var, value)
       end
     end
