dcf9eabb16fd3415ad58b12b52251bdb137c7a91
Chanakya <chanakya.valluri@gmail.com>
2019-09-24 14:10:06 -0700

    custom attr_accessor_objects done

--- W6D2/ActiveRecordPart1/lib/00_attr_accessor_object.rb
+++ W6D2/ActiveRecordPart1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      inst_var = '@'+name.to_s
+
+      define_method(name) do # Getters
+        instance_variable_get(inst_var)
+      end
+
+      sttr_name = (name.to_s + '=').to_sym
+      define_method(sttr_name) do |value| # Setters
+        instance_variable_set(inst_var, value)
+      end
+    end
   end
 end
