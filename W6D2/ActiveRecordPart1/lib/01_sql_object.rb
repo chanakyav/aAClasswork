7009ab22519ea003cc2f3c47aff0d8de544e67c9
Chanakya <chanakya.valluri@gmail.com>
2019-09-24 15:13:22 -0700

    after finalize

--- W6D2/ActiveRecordPart1/lib/01_sql_object.rb
+++ W6D2/ActiveRecordPart1/lib/01_sql_object.rb
@@ -5,18 +5,39 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    if @columns.nil?
+      columns = DBConnection.execute2(<<-SQL).first
+        SELECT *
+        FROM #{self.table_name}
+        LIMIT 1
+      SQL
+      @columns = columns.map(&:to_sym)
+    end
+    return @columns
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      inst_var = '@'+name.to_s
+
+      define_method(name) do                # Getters
+        instance_variable_get(inst_var)
+      end
+
+      sttr_name = (name.to_s + '=').to_sym
+      define_method(sttr_name) do |value|   # Setters
+        instance_variable_set(inst_var, value)
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    return self.name.tableize if @table_name.nil?
+    @table_name
   end
 
   def self.all
@@ -36,7 +57,8 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes = {} if @attributes.nil?
+    return @attributes
   end
 
   def attribute_values
